bc570721c0f6e22d3143350fd8e2a2c3c6aa3106
subzero <shubham8111994@gmail.com>
2023-02-09 23:41:03 +0530

    added emp check feature

--- src/EmployeeWageMain.java
+++ src/EmployeeWageMain.java
@@ -1,5 +1,9 @@
 public class EmployeeWageMain {
+    public static int empCheck(){
+        int empCheck = (int) Math.floor((Math.random() * 10) % 3);
+        return empCheck;
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+        empCheck();
     }
 }

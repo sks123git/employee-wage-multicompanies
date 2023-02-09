207eb69e8bff8485b1ca0661ace0bb5f9dfc2167
subzero <shubham8111994@gmail.com>
2023-02-10 00:45:56 +0530

    added monthly wage calculation feature

--- src/EmployeeWageMain.java
+++ src/EmployeeWageMain.java
@@ -2,13 +2,18 @@ public class EmployeeWageMain {
     public static final int IS_FULL_TIME = 1;
     public static final int EMP_RATE_PER_HOUR = 20;
     public static final int IS_PART_TIME = 2;
+    public static final int NO_OF_WORKING_DAYS = 20;
     public static int empCheck(){   // Random checking employee attendance
         int empCheck = (int) Math.floor((Math.random() * 10) % 3);
         return empCheck;
     }
     public int dailyWageOfEmployee() {
         int empHrs = 0;
+        int totalHrs = 0;
         int dailyWage = 0;
+        int totalWorkingDays = 0;
+        while (totalWorkingDays < NO_OF_WORKING_DAYS) {
+            totalWorkingDays++;
         switch (empCheck()) {
             case IS_PART_TIME: //checking if part time or not
                 empHrs = 4;
@@ -19,12 +24,14 @@ public class EmployeeWageMain {
             default:
                 empHrs = 0;
         }
-        dailyWage = empHrs * EMP_RATE_PER_HOUR;
+        totalHrs+=empHrs;
+        System.out.println("Day#: " + totalWorkingDays + " " + "Emp hr: " + totalHrs);
+        }
         return dailyWage;
     }
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
         EmployeeWageMain employee = new EmployeeWageMain();
-        System.out.println(employee.dailyWageOfEmployee());
+        employee.dailyWageOfEmployee();
     }
 }

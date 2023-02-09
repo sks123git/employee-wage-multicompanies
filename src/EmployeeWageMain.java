9882d7c66d07e303853e37ea34ae894c2fe1e2a2
subzero <shubham8111994@gmail.com>
2023-02-10 00:01:36 +0530

    added daily wage calculation faature

--- src/EmployeeWageMain.java
+++ src/EmployeeWageMain.java
@@ -1,9 +1,26 @@
 public class EmployeeWageMain {
+    public static final int IS_FULL_TIME = 1;
+    public static final int EMP_RATE_PER_HOUR = 20;
     public static int empCheck(){
         int empCheck = (int) Math.floor((Math.random() * 10) % 3);
         return empCheck;
     }
+
+    public int dailyWageOfEmployee() {
+        int empHrs = 0;
+        int dailyWage = 0;
+        switch (empCheck()) {
+            case IS_FULL_TIME: //checking if full time or not
+                empHrs = 8;
+                break;
+            default:
+                empHrs = 0;
+        }
+        dailyWage = empHrs * EMP_RATE_PER_HOUR;
+        return dailyWage;
+    }
     public static void main(String[] args) {
-        empCheck();
+        EmployeeWageMain employee = new EmployeeWageMain();
+        System.out.println(employee.dailyWageOfEmployee());
     }
 }

9f760129bd45078474e08577cb9eb057f39a0c1e
subzero <shubham8111994@gmail.com>
2023-02-10 00:11:46 +0530

    added Part time employee daily wage feature

--- src/EmployeeWageMain.java
+++ src/EmployeeWageMain.java
@@ -1,15 +1,18 @@
 public class EmployeeWageMain {
     public static final int IS_FULL_TIME = 1;
     public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int IS_PART_TIME = 2;
     public static int empCheck(){   // Random checking employee attendance
         int empCheck = (int) Math.floor((Math.random() * 10) % 3);
         return empCheck;
     }
-
     public int dailyWageOfEmployee() {
         int empHrs = 0;
         int dailyWage = 0;
         switch (empCheck()) {
+            case IS_PART_TIME: //checking if part time or not
+                empHrs = 4;
+                break;
             case IS_FULL_TIME: //checking if full time or not
                 empHrs = 8;
                 break;
@@ -20,6 +23,7 @@ public class EmployeeWageMain {
         return dailyWage;
     }
     public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
         EmployeeWageMain employee = new EmployeeWageMain();
         System.out.println(employee.dailyWageOfEmployee());
     }
